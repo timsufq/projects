3ef73831eb0b5a8baf94a6c8cb0ae00b3db3cb50
Fangqiu Su <virtimmy@hotmail.com>
2020-05-14 01:19:20 -0400

    Add files via upload

---
+++ programming/python/sqrt(2)_calculation.py
@@ -0,0 +1,17 @@
+from decimal import *
+getcontext().prec=64;
+n=1
+d=1
+n0=1
+d0=1
+r=Decimal(n/d)
+i=0
+while i<6:
+	n0=n*n+2*d*d
+	d0=2*n*d
+	r=Decimal(n0/d0)
+	print("A result of %d/%d is:"%(n0,d0))
+	print(r)
+	n=n0
+	d=d0
+	i+=1
